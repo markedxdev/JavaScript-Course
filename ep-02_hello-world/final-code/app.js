3078e4b907de2d7d90e6e14e5707282aaacebec2
Ankit Kumar <markedxdev@gmail.com>
2025-05-02 11:22:42 +0530

    ep02 tak ka notes

---
+++ ep-02_hello-world/final-code/app.js
@@ -0,0 +1,24 @@
+// 📌 JavaScript Kya Hai?
+// JavaScript ek programming language hai jo mostly web pages ko interactive banane ke liye use hoti hai.
+
+// 🔹 HTML => Structure banata hai
+// 🔹 CSS => Design/Style deta hai
+// 🔹 JavaScript => Web page ko dynamic banata hai (jaise button click, popup, animations, etc.)
+
+// 🖥️ JavaScript Kis Tarah Se Use Hoti Hai?
+// Client-side JavaScript – Browser ke andar chalta hai (jaise Chrome, Firefox).
+
+// Server-side JavaScript – Backend me bhi chal sakta hai (Node.js ke through).
+
+// 🔧 JavaScript Use Kahan-Kahan Hota Hai?
+// Web pages interactive banane ke liye
+
+// Form validation (e.g. email sahi hai ya nahi)
+
+// Games banane ke liye
+
+// Backend development (Node.js)
+
+// Mobile apps (React Native)
+
+// Desktop apps (Electron.js) 
